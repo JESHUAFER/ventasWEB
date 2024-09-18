07dc0237bd8f7ee881506d79bcf40e5d077adef0
Cristhofer Teni <ctenil@miumg.edu.gt>
2024-09-17 23:55:14 -0600

    :safety_vest:Login Basico Conexion BD

---
+++ Models/AppUsuarios.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ventasWEB.Models
+{
+    public class AppUsuarios : IdentityUser
+    {
+        [Required(ErrorMessage =" El rol es obligatorio")]
+        public string Rol { get; set; }
+        
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ventasWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
